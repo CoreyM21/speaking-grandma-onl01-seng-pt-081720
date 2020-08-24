1938db1afb6075ea97f3ff59f3f475fdcd43a3a0
Corey Martin <coreyjmartin2@gmail.com>
2020-08-24 04:14:29 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -3,9 +3,9 @@
 def speak_to_grandma(phrase)
   if phrase == "I LOVE YOU GRANDMA!"
     "I LOVE YOU TOO PUMPKIN!"
-  elsif phrase == phase.downcase
+  elsif phrase == phrase.downcase
     "HUH?! SPEAK UP, SONNY!"
-    elsif phrase == phase.downcase
+    elsif phrase == phrase.downcase
     "HUH?! SPEAK UP, SONNY!"
   elsif phrase == phrase.upcase
     "NO, NOT SINCE 1938!"
