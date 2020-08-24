90e334a37146fabef5c8b24445b234e1385df50a
Corey Martin <coreyjmartin2@gmail.com>
2020-08-24 04:18:06 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -5,7 +5,7 @@ def speak_to_grandma(phrase)
     "I LOVE YOU TOO PUMPKIN!"
   elsif phrase == phrase.downcase
     "HUH?! SPEAK UP, SONNY!"
-    elsif phrase == phrase.downcase
+  elsif phrase == phrase.downcase
     "HUH?! SPEAK UP, SONNY!"
   elsif phrase == phrase.upcase
     "NO, NOT SINCE 1938!"
